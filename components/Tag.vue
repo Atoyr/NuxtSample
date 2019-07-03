d40bcd41d51296a6dacd799e0a0fbfe06807fa90
Atoyr <lessironglance@gmail.com>
2019-07-03 19:20:41 +0900

    add tags

---
+++ components/Tag.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <p><img :src='icon_url' />  ! </p>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item : Object
+  }
+}
+</script>
---
+++ pages/tags/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <div>
+      <h3>タグ一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <Tag item=item />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import Tag from '~/components/tag.vue';
+export default {
+  async asyncData({ app ,params}) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:' + params.tag)
+    return {
+      items,
+      tag : params.tag
+    }
+  }
+}
+</script>
